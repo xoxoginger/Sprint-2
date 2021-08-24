239d73673378135205c150d766f611fbe0555c1c
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-08-24 20:27:25 +0300

    solved ProblemC

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -2,7 +2,15 @@ class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val n = readInt()
+        val inputData: List<Int> = readListInt().sorted()
+        var numberOfTasks = 0
+
+        for(i in 1 until n step 2) {
+            numberOfTasks += (inputData[i] - inputData[i-1])
+        }
+
+        println(numberOfTasks)
 
     }
 
