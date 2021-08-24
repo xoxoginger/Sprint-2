b919645c41e462d19e7d5bd2493d448e6f1868a6
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-08-24 16:07:20 +0300

    solved ProblemB

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,9 +1,29 @@
 class ProblemB {
 
     fun main() {
+        val numOfQueries = readInt()
+        var outputData: MutableList<String> = mutableListOf<String>()
 
-        //ваша реализация в методе main
+        for (i in 0 until numOfQueries) {
+            val inputData: List<Int> = readListInt()
+            val n = inputData[0]
+            val k = inputData[1]
+            val charCode = 'a'.toByte().toInt()
+            var charCounter = 0
+            var str = ""
 
+            for (j in 0 until n) {
+                str += (charCode + charCounter).toChar()
+                if (charCounter < k - 1)
+                    charCounter++
+                else
+                    charCounter = 0
+            }
+            outputData.add(str)
+        }
+
+        for(i in 0 until numOfQueries)
+            println(outputData[i])
     }
 
 }
