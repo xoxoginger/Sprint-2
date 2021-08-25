8f862462dcacf13a149ba391f90dd57f0b4f8e34
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-08-25 15:17:28 +0300

    not effective, but solved ProblemD

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -2,7 +2,24 @@ class ProblemD {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val numOfQueries = readInt()
+    for(i in 0 until numOfQueries) {
+        val numberOfClients = readInt()
+        val inputData: List<Long> = readListLong()
+        var m = 0L
+
+        for(j in 0 until numberOfClients) {
+            var tempM = 0L
+            for(k in 0 until numberOfClients) {
+                if(inputData[k] >= inputData[j])
+                    tempM += inputData[j]
+            }
+            if(m < tempM)
+                m = tempM
+        }
+        println(m)
+    }
+
 
     }
 
