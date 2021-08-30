64db7c81d28a37b62e2bb4358c38363fda4ab9ea
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-08-30 18:48:37 +0300

    fixed ProblemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -5,21 +5,19 @@ class ProblemA {
         var coordinate = 0L
         var outputData: MutableList<Long> = mutableListOf<Long>()
 
-        for(i in 0..numOfQueries - 1) {
+        for(i in 0 until numOfQueries) {
             val inputData: List<Long> = readListLong()
             val a = inputData[0]
             val b = inputData[1]
             val k = inputData[2]
 
-            if(k % 2 == 0L)
-                coordinate = a * (k / 2) - b * (k / 2)
-            else
-                coordinate = a * ((k + 1) / 2) - b * ((k - 1) / 2)
+            coordinate = if(k % 2 == 0L) (a * (k / 2) - b * (k / 2))
+                         else (a * ((k + 1) / 2) - b * ((k - 1) / 2))
 
             outputData.add(coordinate)
         }
 
-        for(i in 0..numOfQueries - 1)
+        for(i in 0 until numOfQueries)
             println(outputData[i])
     }
 
